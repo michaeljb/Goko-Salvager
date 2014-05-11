413b6173e0b83b3a1837c7169f5e5b6627ebdc20
Michael Brandt <mbrandt@colorado.edu>
2014-05-11 01:43:15 -0600

    modify the 'Always Stack' module to work without overriding Goko methods, instead listening for native Goko events, as suggested by #217; add method whenGameClientReady to listen for the availability of an instance of DominionClient

--- src/ext/alwaysStack.js
+++ src/ext/alwaysStack.js
@@ -1,15 +1,37 @@
 (function () {
-    "use strict";
+    'use strict';
 
     console.log('Loading Always Stack');
 
-    var mod = GS.modules.alwaysStack= new GS.Module('Always Stack');
-    mod.dependencies = ['GS', 'FS.Cards.CardStackPanel'];
+    var mod = GS.modules.alwaysStack = new GS.Module('Always Stack');
+    mod.dependencies = ['GS'];
     mod.load = function () {
-        GS.alsoDo(FS.Cards.CardStackPanel, 'addView', null, function (view, index) {
-            if (GS.get_option('always_stack') && this.autoStackCards) {
-                this.stackCards = true;
+        var alwaysStack;
+
+        GS.whenGameClientReady(function (gameClient) {
+            if (GS.get_option('always_stack')) {
+                gameClient.clientConnection.bind('moveCards', alwaysStack);
             }
         });
+
+        alwaysStack = function () {
+            var dominionWindow,
+                fanPanels;
+
+            dominionWindow = this.gameInstance.playerController.dominionWindow;
+
+            // select the panels used to display cards in a fanned-out manner,
+            // i.e., the panels used to display the cards in hand
+            fanPanels = _.select(dominionWindow.panels, function (panel) {
+                return panel instanceof FS.Cards.FanCardStackPanel;
+            }, this);
+
+            // default value for numCardsAutoStackTrigger is 10; set to 0 so
+            // auto-stacking is always triggered
+            _.each(fanPanels, function (panel) {
+                panel.numCardsAutoStackTrigger = 0;
+            }, this);
+        };
+
     };
 }());
--- src/ext/utils.js
+++ src/ext/utils.js
@@ -165,4 +165,20 @@
         return level;
     };
 
+    // first bind to gameServerHello so an instance of DominionClient is
+    // available, make sure it's bound to the client associated with the current
+    // game
+    //
+    // TODO: test this with multiple tabs starting different games in the same
+    // browser
+    GS.whenGameClientReady = function (callback) {
+        mtgRoom.conn.bind('gameServerHello', function (msg) {
+            var gameClient = _.find(mtgRoom.games, function (game) {
+                return game.gameAddress === msg.data.gameServerAddress;
+            }, this);
+            callback(gameClient);
+        });
+    };
+
+
 }());
